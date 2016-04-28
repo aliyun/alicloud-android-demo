2910d344d8cbb86c7ab400b06dc9bcaa3d528216
lingming.yb <lingming.yb@alibaba-inc.com>
2016-04-28 22:17:12 +0800

    add mpush demo

---
+++ mpush_android_demo/src/main/java/com/alibaba/cloudpushdemo/bizactivity/HelperActivity.java
@@ -0,0 +1,54 @@
+package com.alibaba.cloudpushdemo.bizactivity;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.alibaba.cloudpushdemo.R;
+
+/**
+ * 帮助中心页面
+ */
+public class HelperActivity extends Activity {
+
+    /**
+     * 用于其他Activty跳转到该Activity
+     * @param context
+     */
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, HelperActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helper_page);
+
+        // ActionBar 回退导航
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ mpush_android_demo/src/main/java/com/alibaba/cloudpushdemo/dao/MessageDao.java
@@ -0,0 +1,90 @@
+package com.alibaba.cloudpushdemo.dao;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.alibaba.cloudpushdemo.component.DatabaseHelper;
+import com.alibaba.cloudpushdemo.entitys.MessageEntity;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author: 正纬
+ * @since: 15/4/9
+ * @version: 1.0
+ * @feature: 推送消息DAO
+ */
+public class MessageDao {
+
+    //数据库相关操作日志tag
+    public static final String DB_LOG = "database";
+
+    private Context context;
+    private Dao<MessageEntity, Integer> MessageEntityOp;
+    private DatabaseHelper helper;
+
+    public MessageDao(Context context) {
+        this.context = context;
+        try {
+            helper = DatabaseHelper.getHelper(context);
+            MessageEntityOp = helper.getDao(MessageEntity.class);
+        } catch (SQLException e) {
+            Log.d(DB_LOG, e.toString());
+        }
+    }
+
+    /**
+     * 增加一条数据
+     *
+     * @param entity
+     */
+    public void add(MessageEntity entity) {
+        try {
+            MessageEntityOp.create(entity);
+            Log.d(DB_LOG, "Insert : " + entity.toString());
+        } catch (SQLException e) {
+            Log.d(DB_LOG, e.toString());
+        }
+
+    }
+
+    /**
+     * 删除单条数据
+     *
+     * @param entity
+     */
+    public void del(MessageEntity entity) {
+        try {
+            MessageEntityOp.delete(entity);
+            Log.d(DB_LOG, "Delete : " + entity.toString());
+        } catch (SQLException e) {
+            Log.d(DB_LOG, e.toString());
+        }
+
+    }
+
+    public void delAll() {
+        for (MessageEntity entity : this.getAll()) {
+            this.del(entity);
+        }
+    }
+
+    /**
+     * 查询全部
+     *
+     * @return
+     */
+    public ArrayList<MessageEntity> getAll() {
+        try {
+            Log.d(DB_LOG, "Query : All");
+            return (new ArrayList<MessageEntity>() {{
+                this.addAll(MessageEntityOp.queryForAll());
+            }});
+        } catch (SQLException e) {
+            Log.d(DB_LOG, e.toString());
+        }
+        return null;
+    }
+}
