c30991b40a212aa2cb39bf45936f1759f2fcb92a
lingkun.lk <lingkun.lk@alibaba-inc.com>
2016-12-28 14:22:37 +0800

    Delete HTTPDNS  WebView Scene.

--- httpdns_android_demo/src/main/java/alibaba/httpdns_android_demo/MainActivity.java
+++ httpdns_android_demo/src/main/java/alibaba/httpdns_android_demo/MainActivity.java
@@ -1,48 +1,28 @@
 package alibaba.httpdns_android_demo;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.alibaba.sdk.android.httpdns.HttpDns;
 import com.alibaba.sdk.android.httpdns.HttpDnsService;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
-    private static final String targetUrl = "http://www.apple.com";
     private static final String TAG = "httpdns_android_demo";
     private static HttpDnsService httpdns;
     public final static String accountID = "139450";
     private Button normalSceneBtn;
     private Button httpsSceneBtn;
     private Button sniSceneBtn;
-    private Button webviewSceneBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         // 初始化httpdns
         httpdns = HttpDns.getService(getApplicationContext(), accountID);
         // 预解析热点域名
@@ -61,13 +40,10 @@ public class MainActivity extends AppCompatActivity {
         normalSceneBtn = (Button) findViewById(R.id.normal_scene_btn);
         httpsSceneBtn = (Button) findViewById(R.id.https_scene_btn);
         sniSceneBtn = (Button) findViewById(R.id.sni_scene_btn);
-        webviewSceneBtn = (Button) findViewById(R.id.webview_scene_btn);
 
         normalSceneBtn.setOnClickListener(sceneClickListener);
         httpsSceneBtn.setOnClickListener(sceneClickListener);
         sniSceneBtn.setOnClickListener(sceneClickListener);
-        webviewSceneBtn.setOnClickListener(sceneClickListener);
-
     }
 
     private View.OnClickListener sceneClickListener = new View.OnClickListener() {
@@ -80,8 +56,6 @@ public class MainActivity extends AppCompatActivity {
                 intent.setClass(getApplicationContext(), HttpsActivity.class);
             } else if (view == sniSceneBtn) {
                 intent.setClass(getApplicationContext(), SNIActivity.class);
-            } else if (view == webviewSceneBtn) {
-                intent.setClass(getApplicationContext(), WebviewActivity.class);
             }
             startActivity(intent);
         }
