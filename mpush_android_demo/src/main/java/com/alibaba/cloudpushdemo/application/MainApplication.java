f412ff5b75d4444beff1f8c49149b8fb72f82f1f
asialee <503361483@qq.com>
2017-07-13 11:00:55 +0800

    delete unuseful code

--- mpush_android_demo/src/main/java/com/alibaba/cloudpushdemo/application/MainApplication.java
+++ mpush_android_demo/src/main/java/com/alibaba/cloudpushdemo/application/MainApplication.java
@@ -16,15 +16,6 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-//        l.b(this);
-//        l.e(this);
-//        l.c(this);
-//        SharedPreferences sp = this.getSharedPreferences(Constants.SP_FILE_NAME, Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sp.edit();
-//        editor.clear();
-//        editor.apply();
-
         initCloudChannel(this);
     }
 
@@ -59,29 +50,8 @@ public class MainApplication extends Application {
             }
         });
 
-        MiPushRegister.register(applicationContext, "2882303761517410754", "5661741045754");
+        MiPushRegister.register(applicationContext, "XIAOMI_ID", "XIAOMI_KEY");
         HuaWeiRegister.register(applicationContext);
-        GcmRegister.register(applicationContext, "1014340708672", "1:1014340708672:android:d078cef4f8b2542a");
-    }
-
-    private void startKeepChannelService(Context applicationContext) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            JobInfo.Builder builder = new JobInfo.Builder(900715, new ComponentName(applicationContext.getPackageName(), AccsJobService.class.getName()));
-//            if (Build.VERSION.SDK_INT >= 24) {
-//                builder.setMinimumLatency(270 * 1000);
-//            } else {
-//                builder.setPeriodic(270 * 1000);  //间隔500毫秒调用onStartJob函数， 500只是为了验证
-//            }
-//            PersistableBundle bundle = new PersistableBundle();
-//            bundle.putString("keep", "enable");
-//            builder.setExtras(bundle);
-//            JobScheduler jobScheduler = (JobScheduler) applicationContext.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-//            int ret = jobScheduler.schedule(builder.build());
-//            Log.d("haha","KeepChannelServiceId:" + ret);
-//            if (ret <= 0) {
-//                Log.e("haha","Start KeepChannelService failed. ErrorCode:" + ret);
-//            }
-//        }
-
+        GcmRegister.register(applicationContext, "send_id", "application_id");
     }
 }
