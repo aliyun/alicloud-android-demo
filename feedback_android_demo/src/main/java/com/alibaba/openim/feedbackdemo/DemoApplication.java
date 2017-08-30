bc5d2ecfe8425449041808f0c18c2d1ff584374a
aptentity <aptentity@gmail.com>
2017-08-30 19:28:01 +0800

    update feedback demo

--- feedback_android_demo/src/main/java/com/alibaba/openim/feedbackdemo/DemoApplication.java
+++ feedback_android_demo/src/main/java/com/alibaba/openim/feedbackdemo/DemoApplication.java
@@ -1,27 +1,34 @@
 package com.alibaba.openim.feedbackdemo;
 
+import java.util.concurrent.Callable;
+
+import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
+import com.alibaba.sdk.android.feedback.impl.IActivityCallback;
+import com.alibaba.sdk.android.feedback.util.ErrorCode;
+import com.alibaba.sdk.android.feedback.util.FeedbackErrorCallback;
+
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
-
-import com.alibaba.mtl.appmonitor.AppMonitor;
-import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
-import com.alibaba.sdk.android.feedback.util.ErrorCode;
-import com.alibaba.sdk.android.feedback.util.FeedbackErrorCallback;
-
-import java.util.HashMap;
-import java.util.concurrent.Callable;
-
+import com.githang.statusbar.StatusBarCompat;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DemoApplication extends Application {
-    public final static String DEFAULT_APPKEY = "your appKey";
-    public final static String DEFAULT_APPSECRET = "your appSecret";
+    //public final static String DEFAULT_APPKEY = "your appKey";
+    //public final static String DEFAULT_APPSECRET = "your appSecret";
+    public final static String DEFAULT_APPKEY = "24603959";
+    public final static String DEFAULT_APPSECRET = "8edf2f14bc21ea91b354d3c5d0f7bc75";
     
     @Override
     public void onCreate() {
         super.onCreate();
-        // 添加自定义的error handler
+
+        /**
+         * 添加自定义的error handler
+         */
         FeedbackAPI.addErrorCallback(new FeedbackErrorCallback() {
             @Override
             public void onError(Context context, String errorMessage, ErrorCode code) {
@@ -36,27 +43,46 @@ public class DemoApplication extends Application {
             }
         });
 
-        //建议放在此处做初始化
+        /**
+         * 建议放在此处做初始化
+         */
         FeedbackAPI.init(this, DEFAULT_APPKEY, DEFAULT_APPSECRET);
 
+        /**
+         * 在Activity的onCreate中执行的代码
+         * 可以设置状态栏背景颜色和图标颜色，这里使用com.githang:status-bar-compat来实现
+         */
+        FeedbackAPI.setActivityCallback(new IActivityCallback() {
+            @Override
+            public void onCreate(Activity activity) {
+                StatusBarCompat.setStatusBarColor(activity,getResources().getColor(R.color.aliwx_setting_bg_nor),true);
+            }
+        });
+
+        /**
+         * 自定义参数演示
+         */
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("loginTime", "登录时间");
+            jsonObject.put("visitPath", "登陆，关于，反馈");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        FeedbackAPI.setAppExtInfo(jsonObject);
+
+        /**
+         * 以下是设置UI
+         */
         //设置默认联系方式
         FeedbackAPI.setDefaultUserContactInfo("13800000000");
-
         //沉浸式任务栏，控制台设置为true之后此方法才能生效
         FeedbackAPI.setTranslucent(true);
-
         //设置返回按钮图标
         FeedbackAPI.setBackIcon(R.drawable.ali_feedback_common_back_btn_bg);
-
         //设置标题栏"历史反馈"的字号，需要将控制台中此字号设置为0
         FeedbackAPI.setHistoryTextSize(20);
-
-
-        // 验证代码
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("debug_api_url","http://muvp.alibaba-inc.com/online/UploadRecords.do");
-        map.put("debug_key","aliyun_sdk_utDetection");
-        //map.put("debug_sampling_option", "true");
-        AppMonitor.turnOnRealTimeDebug(map);
+        //设置标题栏高度，单位为像素
+        FeedbackAPI.setTitleBarHeight(100);
     }
 }
--- feedback_android_demo/src/main/java/com/alibaba/openim/feedbackdemo/DemoLoginActivity.java
+++ feedback_android_demo/src/main/java/com/alibaba/openim/feedbackdemo/DemoLoginActivity.java
@@ -1,5 +1,11 @@
 package com.alibaba.openim.feedbackdemo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
+import com.alibaba.sdk.android.feedback.util.IUnreadCountCallback;
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
@@ -10,94 +16,77 @@ import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
-import com.alibaba.sdk.android.feedback.util.IUnreadCountCallback;
-
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class DemoLoginActivity extends Activity implements OnClickListener {
     private final static String TAG = "DemoLoginActivity";
-    private Button loginButton;
-    private LinearLayout unreadCount;
-    private LinearLayout scan;
-    private TextView appkeyTextView;
-    private int clickCount = 0;
-    private boolean isGetting = false;
+    private View btnOpenActivity,btnOpenFragment,btnGetUnreadCount,btnScan;
+    private TextView tvConsoleText;
 
     private static final int CAMERA_PERMISSIONS = 1;
     private static final int STORAGE_AND_CAMERA_PERMISSIONS = 2;
 
     private Handler handler = new Handler(Looper.getMainLooper());
 
-    private boolean isOpen = false;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_login);
 
-        appkeyTextView = (TextView)findViewById(R.id.appkey);
-        appkeyTextView.setText("AppKey:" + DemoApplication.DEFAULT_APPKEY);
-        //自定义参数演示
-        /*JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("loginTime", "登录时间");
-            jsonObject.put("visitPath", "登陆，关于，反馈");
-        } catch (JSONException e) {
-            e.printStackTrace();
+        tvConsoleText = (TextView)findViewById(R.id.tvConsoleText);
+        btnOpenActivity = findViewById(R.id.btnOpenActivity);
+        btnGetUnreadCount = findViewById(R.id.btnUnread);
+        btnOpenFragment = findViewById(R.id.btnOpenFragment);
+        btnScan = findViewById(R.id.btnScan);
+
+        btnOpenActivity.setOnClickListener(this);
+        btnGetUnreadCount.setOnClickListener(this);
+        btnOpenFragment.setOnClickListener(this);
+        btnScan.setOnClickListener(this);
+
+        tvConsoleText.append("AppKey:" + DemoApplication.DEFAULT_APPKEY+"\n");
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnOpenActivity:
+                tvConsoleText.append("open feedback activity\n");
+                checkForOpenOrGet(true);
+                break;
+            case R.id.btnUnread:
+                getFeedbackUnreadCount();
+                break;
+            case R.id.btnScan:
+                tvConsoleText.append("open scan\n");
+                checkForScan();
+                break;
+            case R.id.btnOpenFragment:
+                tvConsoleText.append("open feedback fragment\n");
+                startActivity(new Intent(this, DemoFragmentActivity.class));
+                break;
+            default:
+                break;
         }
-        FeedbackAPI.setAppExtInfo(jsonObject);*/
-
-        loginButton = (Button)findViewById(R.id.login);
-        unreadCount = (LinearLayout)findViewById(R.id.unreadCountGroup);
-        scan = (LinearLayout)findViewById(R.id.scanGroup);
-
-        loginButton.setOnClickListener(this);
-        scan.setOnClickListener(this);
-        unreadCount.setOnClickListener(this);
-
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data); //49374
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
-                isGetting = false;
                 Log.e(TAG, "scan getContents is null");
             } else {
                 String url = result.getContents();
                 Log.d(TAG, "scan getContents: " + url);
-
-                //                String[] temp = url.split("appkey=");
-                //                String[] contents = temp[1].split("#");
-                //                String key = contents[0];
-                //
-                //                FeedbackAPI.mFeedbackCustomInfoMap.put("appkey", key);
-                // note:
-                // 此处由于预览提供的是http的url，但是线上必须是https的，所以这里需要hack一下
-                // 为什么不是服务端改呢，这是为什么呢。。。
-                // 我只是个android开发, 谢谢
                 String pattern = "^(https?)";
                 Pattern regex = Pattern.compile(pattern);
                 Matcher m = regex.matcher(url);
@@ -111,40 +100,16 @@ public class DemoLoginActivity extends Activity implements OnClickListener {
                     @Override
                     public void run() {
                         FeedbackAPI.openFeedbackActivity();
-                        isGetting = false;
                     }
                 }, 500);
             }
         } else {
-            isGetting = false;
             // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    @Override
-    public void onClick(View view) {
-        //防止重复点击, 打开多次问题
-        if (isGetting) {
-            return;
-        }
-        isGetting = true;
-        switch (view.getId()) {
-            case R.id.login:
-                isOpen = true;
-                checkForOpenOrGet(true);
-                break;
-            case R.id.unreadCountGroup:
-                isOpen = false;
-                checkForOpenOrGet(false);
-                break;
-            case R.id.scanGroup:
-                checkForScan();
-                break;
-            default:
-                break;
-        }
-    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -152,15 +117,15 @@ public class DemoLoginActivity extends Activity implements OnClickListener {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 scan();
             } else {
-                isGetting = false;
                 Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+                tvConsoleText.append("Camera Permission Denied\n");
             }
         } else if (requestCode == STORAGE_AND_CAMERA_PERMISSIONS) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openOrGet(isOpen);
+                openOrGet(true);
             } else {
-                isGetting = false;
                 Toast.makeText(this, "Storge or Camera Permission Denied", Toast.LENGTH_SHORT).show();
+                tvConsoleText.append("Storge or Camera Permission Denied\n");
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -206,45 +171,59 @@ public class DemoLoginActivity extends Activity implements OnClickListener {
         //接入方不需要这样调用, 因为扫码预览, 同时为了服务器发布后能做到实时预览效果, 所有每次都init.
         //业务方默认只需要init一次, 然后直接openFeedbackActivity/getFeedbackUnreadCount即可
         FeedbackAPI.init(this.getApplication(), DemoApplication.DEFAULT_APPKEY, DemoApplication.DEFAULT_APPSECRET);
-        final Activity context = this;
-        //如果500ms内init未完成, openFeedbackActivity会失败, 可以延长时间>500ms
+
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (isOpenFeedback) {
                     FeedbackAPI.openFeedbackActivity();
-                } else {
-                    FeedbackAPI.getFeedbackUnreadCount(new IUnreadCountCallback() {
-                        @Override
-                        public void onSuccess(final int unreadCount) {
-                            handler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast toast = Toast.makeText(DemoLoginActivity.this, "未读数：" + unreadCount,
-                                        Toast.LENGTH_SHORT);
-                                    toast.show();
-                                    isGetting = false;
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onError(int i, String s) {
-
-                        }
-                    });
                 }
-                isGetting = false;
             }
         }, 500);
     }
 
-    //连续5次点击首页"用户反馈"标题, fragment方式进入反馈页面
-    public void openFeedbackFragment(View view) {
-        clickCount++;
-        if (clickCount == 5) {
-            clickCount = 0;
-            startActivity(new Intent(this, DemoFragmentActivity.class));
+    private void getFeedbackUnreadCount(){
+        tvConsoleText.append("get unread count\n");
+        FeedbackAPI.getFeedbackUnreadCount(new IUnreadCountCallback() {
+            @Override
+            public void onSuccess(final int unreadCount) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast toast = Toast.makeText(DemoLoginActivity.this, "未读数：" + unreadCount,
+                            Toast.LENGTH_SHORT);
+                        toast.show();
+                        tvConsoleText.append("get unread count success. unreadCount = "+unreadCount+"\n");
+                    }
+                });
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                tvConsoleText.append("get unread count failed:. code="+i+" msg="+s+"\n");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_about_us:
+                AboutActivity.actionStart(this.getApplicationContext());
+                return true;
+            case R.id.action_helper:
+                HelperActivity.actionStart(this.getApplicationContext());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
