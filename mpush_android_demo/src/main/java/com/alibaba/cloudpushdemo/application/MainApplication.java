413ad63848d06ad168ca5eb922f43e58baca39bb
asialee <503361483@qq.com>
2018-02-27 14:27:18 +0800

    接入push3.1.1版本，适配notificaiton channel

--- mpush_android_demo/src/main/java/com/alibaba/cloudpushdemo/application/MainApplication.java
+++ mpush_android_demo/src/main/java/com/alibaba/cloudpushdemo/application/MainApplication.java
@@ -1,7 +1,11 @@
 package com.alibaba.cloudpushdemo.application;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.Log;
 
 import com.alibaba.cloudpushdemo.bizactivity.MainActivity;
@@ -26,6 +30,27 @@ public class MainApplication extends Application {
      * @param applicationContext
      */
     private void initCloudChannel(final Context applicationContext) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // 通知渠道的id
+            String id = "1";
+            // 用户可以看到的通知渠道的名字.
+            CharSequence name = "notification channel";
+            // 用户可以看到的通知渠道的描述
+            String description = "notification description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // 配置通知渠道的属性
+            mChannel.setDescription(description);
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //最后在notificationmanager中创建该通知渠道
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
         PushServiceFactory.init(applicationContext);
         final CloudPushService pushService = PushServiceFactory.getCloudPushService();
         pushService.register(applicationContext, new CommonCallback() {
@@ -44,7 +69,7 @@ public class MainApplication extends Application {
 
         MiPushRegister.register(applicationContext, "XIAOMI_ID", "XIAOMI_KEY"); // 初始化小米辅助推送
         HuaWeiRegister.register(applicationContext); // 接入华为辅助推送
-        GcmRegister.register(applicationContext, "send_id", "application_id"); // 接入FCM/GCM初始化推送
+        //GcmRegister.register(applicationContext, "send_id", "application_id"); // 接入FCM/GCM初始化推送
     }
 
     public static void setMainActivity(MainActivity activity) {
