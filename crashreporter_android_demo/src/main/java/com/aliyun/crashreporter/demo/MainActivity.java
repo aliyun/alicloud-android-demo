4b217e8fb9d49e37f252450f11511b8f00f8777c
hailong.lhl <hailong.lhl@alibaba-inc.com>
2019-08-22 19:34:31 +0800

    1、新增crashreport demo； 2、更新push demo到最新版本。

---
+++ crashreporter_android_demo/src/main/java/com/aliyun/crashreporter/demo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.aliyun.crashreporter.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import crashreporter.motu.alibaba.com.tbcrashreporter4androiddemo.NativeCrashTest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // java crash
+        Button javaCrashBtn = (Button) findViewById(R.id.javaCrash);
+        javaCrashBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                throw new NullPointerException();
+            }
+        });
+
+        //native crash test
+        final NativeCrashTest nativeCrashTest = new NativeCrashTest();
+        // native crash
+        Button nativeCrashBtn = (Button) findViewById(R.id.nativeCrash);
+        nativeCrashBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nativeCrashTest.TestNativeCrashMethod(1);
+            }
+        });
+
+        //卡顿
+        Button stuckCrashBtn = (Button) findViewById(R.id.stuck);
+        stuckCrashBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Thread.sleep(20*1000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
