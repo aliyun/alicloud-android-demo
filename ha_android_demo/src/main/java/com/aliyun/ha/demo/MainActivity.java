494697625bdfd57c1ba87099198ac453e8c99e60
任伟 <rw01965401@alibaba-inc.com>
2024-08-01 15:30:13 +0800

    feat: 解决升级gradle后,构建失败的问题

--- ha_android_demo/src/main/java/com/aliyun/ha/demo/MainActivity.java
+++ ha_android_demo/src/main/java/com/aliyun/ha/demo/MainActivity.java
@@ -2,11 +2,12 @@ package com.aliyun.ha.demo;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.alibaba.ha.adapter.AliHaAdapter;
 import com.alibaba.ha.adapter.service.tlog.TLogService;
