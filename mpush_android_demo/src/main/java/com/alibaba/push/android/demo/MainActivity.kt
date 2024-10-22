966cad8721e52d7754dfb6855415be2ffd2989a2
任伟 <rw01965401@alibaba-inc.com>
2024-10-22 18:09:18 +0800

    feat: 首页框架优化

--- mpush_android_demo/src/main/java/com/alibaba/push/android/demo/MainActivity.kt
+++ mpush_android_demo/src/main/java/com/alibaba/push/android/demo/MainActivity.kt
@@ -6,11 +6,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.alibaba.push.android.demo.databinding.MainActivityBinding
@@ -24,7 +22,6 @@ import com.alibaba.push.android.demo.databinding.MainActivityBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
-    private lateinit var viewModel: MainViewModel
 
     private var mBackKeyPressedTime = 0L
 
@@ -45,10 +42,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         //浸入状态栏
         val controller = WindowCompat.getInsetsController(window, window.decorView)
-//        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         controller.isAppearanceLightStatusBars = true
         //设置状态栏透明
         window.statusBarColor = Color.TRANSPARENT
@@ -56,22 +52,43 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         binding.lifecycleOwner = this
         setContentView(binding.root)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        binding.viewModel = viewModel
+
         binding.viewPager.adapter = MainFragmentStateAdapter(
             this,
-            mutableListOf(BasicFuncFragment(), AdvancedFuncFragment(), InfoFragment())
+            mutableListOf(
+                BasicFuncFragment(),
+                AdvancedFuncFragment(),
+                InfoFragment())
         )
+        binding.navView.itemIconTintList = null
+        binding.navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_basic -> {
+                    it.setChecked(true)
+                    binding.viewPager.setCurrentItem(0, false)
+                }
+                R.id.navigation_advance -> {
+                    it.setChecked(true)
+                    binding.viewPager.setCurrentItem(1, false)
+                }
+                R.id.navigation_info -> {
+                    it.setChecked(true)
+                    binding.viewPager.setCurrentItem(2, false)
+                }
+            }
+            false
+        }
 
         binding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
-                viewModel.fragmentIndex.value = position
+                binding.navView.selectedItemId = when (position) {
+                    0 -> R.id.navigation_basic
+                    1 -> R.id.navigation_advance
+                    2 -> R.id.navigation_info
+                    else ->  R.id.navigation_basic
+                }
             }
         })
-
-        viewModel.tabClickCallBack = {
-            binding.viewPager.currentItem = it
-        }
     }
 
     override fun onBackPressed() {
